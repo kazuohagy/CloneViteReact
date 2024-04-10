1017510aa996aec75f489b03fcdd759861877662
Gustavo Kazuo <kazuo.hagy@gmail.com>
2024-04-10 09:14:48 -0300

    Bom dia

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { useState } from 'react'
 import {Logo} from './components/logo.tsx'
+import {Arrow} from './components/arrow.tsx'
 import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <main>
-      <header>
-       <div className="logo-container">
+    <main >
+      <header className='header-container'>
           <Logo/>
-        
-        </div>
+          <div className="menu-options">
+            <a>What we offer</a>
+            <a>How it works</a>
+            <a>Portfolio</a>
+          </div>
+          <button>Contact us
+            <Arrow />
+          </button>
       </header>
     </main>
   )
