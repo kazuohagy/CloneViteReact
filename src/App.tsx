2737308d8ef05abcad380d89731ea94b8f5123be
Gustavo Kazuo <kazuo.hagy@gmail.com>
2024-04-15 09:33:26 -0300

    foi os icones

--- src/App.tsx
+++ src/App.tsx
@@ -28,12 +28,21 @@ function App() {
       </header>
       <div className="content-container">
         <h1>Get the best <span>design</span> for your website</h1>
-        <div className="bubble_target" >
-          😺
+        <div className="bubble target" >
+          <img src="https://assets-global.website-files.com/63d0f8b43ca85422d48a69d6/63d187a9967765985b4ada30_direct-hit.png" style={{ width: 70, height: 70 }} loading="lazy" alt="" />
         </div>
-        <div className="bubble_target" >
-          😺
+        <div className="bubble shop" >
+          <img src="https://assets-global.website-files.com/63d0f8b43ca85422d48a69d6/63d187a8a7449241d438c9b9_shopping-bags.png" style={{ width: 24, height: 24 }} loading="lazy" alt="" />
+        </div>        
+        <div className="bubble rocket" >
+          <img src="https://assets-global.website-files.com/63d0f8b43ca85422d48a69d6/63d187a871aadbac60940662_rocket.png" style={{ width: 40, height: 40 }}  loading="lazy" alt="" />
+        </div>        
+        <div className="bubble fire" >
+          <img src="https://assets-global.website-files.com/63d0f8b43ca85422d48a69d6/63d1807dd0496a3408c33ef6_fire.png" style={{ width: 24, height: 24 }} loading="lazy" alt=""  />
         </div>
+        {/* <div className="bubble cat" >
+          😺
+        </div> */}
       </div>
     </main>
   )
